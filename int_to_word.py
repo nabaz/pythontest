989ccbb6ede6fc1558967b788f7e69ba9aecd4d6
Nabaz Maaruf <nabaz@idevelopwebsite.com>
2017-09-19 11:45:21 -0400

    fixed bug

--- int_to_word.py
+++ int_to_word.py
@@ -20,12 +20,11 @@ def normal(number):
     word_key_map = {'1': "one", '2': "two", '3': "three", '4': "four", '5': "five", '6': "six",
         '7': "seven", '8': "eight", '9': "nine", '0': "zero"}
     number_scope_map = {'1' : '', '2':'tens', '3': 'hundred', '4': 'thousand', '7': 'millions', '10': 'billions'}
-    tens_map = {'10': 'ten', '20': 'twenty', '30':'thirty', '40' :'fourty', '50': 'fifty', '60':'sixty', ''}
+    tens_map = {'10': 'ten', '20': 'twenty', '30':'thirty', '40' :'fourty', '50': 'fifty', '60':'sixty', '70':'seventy'}
     a_list = Deque()
     for i in str(number):
         a_list.add_front(i)
-
-    for i in a_list:
+    for i in str(a_list):
         if i in word_key_map:
              print(word_key_map.get(i))
 
--- test/anagram.py
+++ test/anagram.py
@@ -2,6 +2,9 @@ def anagram_s1(s1, s2):
     a_list = s2
     pos1 = 0
     still_ok = True
-    while pos1 < len(s1) and not found:
+    while pos1 < len(s1) and not still_ok:
         pos2 = 2
-        found = False
+        still_ok = False
+
+
+print anagram_s1('level', 'level')
